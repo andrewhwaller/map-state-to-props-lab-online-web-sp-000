df05952438a4a8fbd5b0d2eb7d8348cc8f433ceb
Andrew Waller <andrewhwaller@gmail.com>
2020-02-22 19:39:17 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -8,11 +8,10 @@ class Users extends Component {
 
     return (
       <div>
-        {this.props.numberOfUsers}
         <ul>
           Users!
-          {/* Write code here that displays the usernames of all users in the Redux store */}
-          {/* In addition, display the total number of users curently in the store */}
+          {this.props.numberOfUsers}
+          {users}
         </ul>
       </div>
     )
